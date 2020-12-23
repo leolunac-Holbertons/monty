41b6f2fee857c72499f74c83b629e8c757edb92e
leolunac-holbertons <2176@leolunac-Holbertons>
2020-12-23 12:08:36 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	fileprocess(file);
+	processfile(file);
 	fclose(file);
 	return (EXIT_SUCCESS);
 }
