65064309d128bcbf6e6f73411e697865b8d3b019
leolunac-holbertons <2176@leolunac-Holbertons>
2020-12-23 12:09:34 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -7,14 +7,14 @@
 #include <stdbool.h>
 #include <ctype.h>
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
+* struct stack_s - doubly linked list representation of a stack (or queue)
+* @n: integer
+* @prev: points to the previous element of the stack (or queue)
+* @next: points to the next element of the stack (or queue)
+*
+* Description: doubly linked list node structure
+* for stack, queues, LIFO, FIFO Holberton project
+*/
 typedef struct stack_s
 {
 	int n;
@@ -23,13 +23,13 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
+* struct instruction_s - opcode and its function
+* @opcode: the opcode
+* @f: function to handle the opcode
+*
+* Description: opcode and its function
+* for stack, queues, LIFO, FIFO Holberton project
+*/
 typedef struct instruction_s
 {
 	char *opcode;
@@ -44,8 +44,13 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void subop(stack_t **stack, unsigned int line_number);
+void divop(stack_t **stack, unsigned int line_number);
+void mulop(stack_t **stack, unsigned int line_number);
+void modop(stack_t **stack, unsigned int line_number);
+
 void free_stack(stack_t **head);
 void get_opcode_func(char *opcde, stack_t **stack, unsigned int line, FILE *f);
-void fileprocess(FILE *file);
+void processfile(FILE *file);
 bool valid_arg(char *arg);
 #endif
