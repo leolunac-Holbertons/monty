34ea23c2c5a3c645684edc071189299598d2b2d1
leolunac-holbertons <2176@leolunac-Holbertons>
2020-12-23 00:12:50 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * main - Entry point
+ * @argc: num arguments
+ * @argv: array with argumente
+ * Description:  return success
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	pfile(file);
+	fclose(file);
+	return (EXIT_SUCCESS);
+}
